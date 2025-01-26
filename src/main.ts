a8981b539ff61efa71f0bd07978988ff3590eaf9
shizakira <ikirachn@gmail.com>
2025-01-26 21:50:11 +0500

    add ElementPlus to main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import VueDraggableResizable from 'vue-draggable-resizable'
 import "vue-draggable-resizable/style.css";
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 
@@ -11,5 +13,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus)
 app.component("vue-draggable-resizable", VueDraggableResizable)
 app.mount('#app')
