219b3b8b7001762ac7cd262ddff5608096194e9d
shizakira <ikirachn@gmail.com>
2025-01-26 20:56:06 +0500

    create a block with width and height

--- src/components/DraggableResizeableImage.vue
+++ src/components/DraggableResizeableImage.vue
@@ -10,7 +10,7 @@ const { getDpi } = useScreenConfigStore()
 const dpi = getDpi()
 const inchCm = 2.54
 
-const pixelSizes = reactive({
+const pixelSizes = reactive<Sizes>({
   width: 500,
   height: 500,
 })
@@ -20,50 +20,52 @@ const cmSizes = reactive<Sizes>({
   height: 0,
 })
 
-const assignSizes = () => {
-  const widthPx = imageElem.value?.width
-  const heightPx = imageElem.value?.height
+const assignCmSizes = () => {
+  pixelSizes.width = imageElem.value?.width as number
+  pixelSizes.height = imageElem.value?.height as number
 
-  cmSizes.width = +(((widthPx as number) / dpi) * inchCm).toFixed(1)
-  cmSizes.height = +(((heightPx as number) / dpi) * inchCm).toFixed(1)
+  cmSizes.width = +((pixelSizes.width / dpi) * inchCm).toFixed(1)
+  cmSizes.height = +((pixelSizes.height / dpi) * inchCm).toFixed(1)
 }
 
+const assignPixelSizes = () => {
+  pixelSizes.width = Math.round((cmSizes.width / inchCm) * dpi)
+  pixelSizes.height = Math.round((cmSizes.height / inchCm) * dpi)
+}
 const handleImageOnLoad = () => {
   if (imageElem.value) {
-    assignSizes()
+    assignCmSizes()
   }
 }
 
-watch(
-  () => cmSizes,
-  () => {
-    pixelSizes.width = Math.round((cmSizes.width / inchCm) * dpi)
-    pixelSizes.height = Math.round((cmSizes.height / inchCm) * dpi)
-  },
-  { deep: true },
-)
+watch(() => cmSizes, assignPixelSizes, { deep: true })
 </script>
 
 <template>
   <div class="image-container">
     <div class="image-sizes">
-      <span>
-        Ширина
-        <el-input v-model="cmSizes.width" style="width: 80px" :placeholder="cmSizes.width" />
-        cм
-      </span>
-      <span>
-        Высота
-        <el-input v-model="cmSizes.height" style="width: 80px" :placeholder="cmSizes.height" />
-        см
-      </span>
+      <div class="length-block">
+
+        <div class="length-elem">
+          <span class="length-word"> Ширина </span>
+          <el-input v-model="cmSizes.width" style="width: 80px" :placeholder="cmSizes.width" />
+          <span>см</span>
+        </div>
+
+        <div class="length-elem">
+          <span class="length-word">Высота</span>
+          <el-input v-model="cmSizes.height" style="width: 80px" :placeholder="cmSizes.height" />
+          <span>см </span>
+        </div>
+
+      </div>
     </div>
     <vue-draggable-resizable
       :x="400"
       :y="70"
       :w="pixelSizes.width"
       :h="pixelSizes.height"
-      @resize="assignSizes"
+      @resize="assignCmSizes"
     >
       <div class="image-wrapper" ref="imageWrapper">
         <img
@@ -78,7 +80,7 @@ watch(
   </div>
 </template>
 
-<style>
+<style scoped lang="scss">
 .image-sizes {
   display: flex;
   flex-direction: column;
@@ -94,4 +96,30 @@ watch(
   height: 100%;
   object-fit: contain;
 }
+
+.length-block{
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  max-width: fit-content;
+
+  padding: 15px;
+  margin-top: 10px;
+
+  border: 1px solid rgb(166.2, 168.6, 173.4);
+  border-radius: 15px;
+
+  .length-elem{
+    display: flex;
+    gap: 10px;
+    align-items: self-start;
+    width: 200px;
+    .length-word {
+      flex: 1;
+    }
+  }
+}
+
+
 </style>
